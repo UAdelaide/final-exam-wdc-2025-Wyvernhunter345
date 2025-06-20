2893d84367b44bb0d0d3ae4d6dda0dc09f2a0805
Wyvernhunter345 <syndix01@outlook.com>
2025-06-20 13:33:08 +0000

    [automatic commit] 2025-06-20 13:33:08 UTC 101.174.33.164

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/dogs', function (req,res,next) {
-    
+    try {
+        await db
+    } catch (e) {
+
+    }
 });
 
 module.exports = router;
