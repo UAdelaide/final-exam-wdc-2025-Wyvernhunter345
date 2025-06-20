b1635d30817db4c94ce3527e463272e8608b7fe6
Wyvernhunter345 <syndix01@outlook.com>
2025-06-20 13:10:01 +0000

    [automatic commit] 2025-06-20 13:10:01 UTC 101.174.33.164

--- part1/app.js
+++ part1/app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql');
 
+var dbConnectionPool = 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
@@ -20,8 +22,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
