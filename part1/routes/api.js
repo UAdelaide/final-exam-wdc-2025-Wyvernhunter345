c950e1c023d012fd5ee625c5fa8681faa5963f38
Wyvernhunter345 <syndix01@outlook.com>
2025-06-20 13:45:31 +0000

    [automatic commit] 2025-06-20 13:45:31 UTC 101.174.33.164

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/dogs', async function (req,res,next) {
     try {
-        const [dogs] = await req.pool.get(`SELECT dog_name, size FROM Dogs`);
+        const [dogs] = await req.pool.getConnection(`SELECT dog_name, size FROM Dogs`);
         return dogs;
     } catch (e) {
         res.status(500).json({ error: 'Failed to fetch books' });
