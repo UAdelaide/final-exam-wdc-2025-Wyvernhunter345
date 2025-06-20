de045cde73699e84ad0cc875d4f4f97bc82825cf
Wyvernhunter345 <syndix01@outlook.com>
2025-06-20 13:27:26 +0000

    [automatic commit] 2025-06-20 13:27:26 UTC 101.174.33.164

--- part1/app.js
+++ part1/app.js
@@ -26,7 +26,15 @@ let dbConnectionPool;
     });
 
     await db.execute(`DROP TABLE IF EXISTS Users`);
-    await db.execute
+    await db.execute(`CREATE TABLE Users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    role ENUM('owner', 'walker') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);`);
+
 
 
   }
