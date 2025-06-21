bd94c99f2a7a76ba3103315ceaf33a2deaf0fd8d
Wyvernhunter345 <syndix01@outlook.com>
2025-06-21 04:43:06 +0000

    [automatic commit] 2025-06-21 04:43:06 UTC 101.174.33.164

--- part2/app.js
+++ part2/app.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
 // Routes
-const walkRoutes = require('./routes/walkRoutes');
-const userRoutes = require('./routes/userRoutes');
-const dogRoutes = require('./routes/dogRoutes');
+const walkRoutes = require("./routes/walkRoutes");
+const userRoutes = require("./routes/userRoutes");
+const dogRoutes = require("./routes/dogRoutes");
 
-app.use('/api/walks', walkRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/dogs', dogRoutes);
+app.use("/api/walks", walkRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/dogs", dogRoutes);
 
 // Export the app instead of listening here
 module.exports = app;
--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../models/db');
+const db = require("../models/db");
 
 // GET list of all registered dogs
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
     try {
-      const [dogs] = await db.execute("SELECT * FROM Dogs");
-      res.json(dogs);
+        const [dogs] = await db.execute("SELECT * FROM Dogs");
+        res.json(dogs);
     } catch (e) {
-      res.status(500).json({ error: 'Failed to fetch users' });
+        res.status(500).json({ error: "Failed to fetch users" });
     }
 });
 
