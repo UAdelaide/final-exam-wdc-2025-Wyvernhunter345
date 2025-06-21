31a9c4725714c5948acd1af751437a85532042a5
Wyvernhunter345 <syndix01@outlook.com>
2025-06-21 06:22:32 +0000

    [automatic commit] 2025-06-21 06:22:32 UTC 101.174.33.164

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -88,7 +88,7 @@ router.post("/login", async (req, res, next) => {
 
 router.get("/logout", async (req, res, next) => {
     // Remove the user's session and sign them out
-    req.session.user = null;
+    req.session.id = null;
     res.redirect("/");
 });
 
