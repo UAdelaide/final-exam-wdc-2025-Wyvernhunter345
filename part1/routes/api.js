9c35632d963c743d39c54417e89971fb72f0b0c8
Wyvernhunter345 <syndix01@outlook.com>
2025-06-21 01:15:06 +0000

    [automatic commit] 2025-06-21 01:15:06 UTC 101.174.33.164

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/dogs', async function (req,res,next) {
     try {
-        const [dogs] = await req.pool.execute('SELECT DISTINCT name, size, Users.user_ AS owner_username FROM Dogs INNER JOIN Users ON Dogs.owner_id=Users.user_id');
+        const [dogs] = await req.pool.execute('SELECT DISTINCT name, size, Users.username AS owner_username FROM Dogs INNER JOIN Users ON Dogs.owner_id=Users.user_id');
         res.json(dogs);
     } catch (e) {
         res.status(500).json({ error: 'Failed to fetch dogs' });
