b38b38259c1cd17c75a22e968a35229a150732d5
Wyvernhunter345 <syndix01@outlook.com>
2025-06-21 05:19:56 +0000

    [automatic commit] 2025-06-21 05:19:56 UTC 101.174.33.164

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -59,7 +59,7 @@ router.post("/login", async (req, res) => {
         if (rows.length === 0) {
             return res.status(401).json({
                 error: "Invalid credentials ",
-                body: JSON.
+                body: JSON.parse(req.body),
             });
         }
 
