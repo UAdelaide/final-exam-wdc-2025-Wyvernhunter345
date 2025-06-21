aff4a0aa2d35e72736979f1bcb0070c79931f7b6
Wyvernhunter345 <syndix01@outlook.com>
2025-06-21 01:55:57 +0000

    [automatic commit] 2025-06-21 01:55:57 UTC 101.174.33.164

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -35,7 +35,7 @@ router.get('/walkers/summary', async function (req,res,next) {
             ON Users.user_id = WalkRatings.walker_id
             INNER JOIN WalkRequests
             ON Users.user_id = WalkRequests.walker_id
-            WHERE 
+            WHERE role = "walker" AND WalkRequests.
             GROUP BY Username`);
         req.json(walkers);
     } catch (e) {
