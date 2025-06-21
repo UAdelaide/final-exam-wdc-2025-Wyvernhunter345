22482604f56ad25aba604f26333465fca966c890
Wyvernhunter345 <syndix01@outlook.com>
2025-06-21 05:05:52 +0000

    [automatic commit] 2025-06-21 05:05:52 UTC 101.174.33.164

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -5,7 +5,7 @@ const db = require("../models/db");
 // GET list of all registered dogs
 router.get("/", async (req, res) => {
     try {
-        const [dogs] = await db.execute("SELECT * FROM Dogs");
+        const [dogs] = await db.execute("SELECT DISTINCT * FROM Dogs");
         res.json(dogs);
     } catch (e) {
         res.status(500).json({ error: "Failed to fetch users" });
