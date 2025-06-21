3bb20894ba0e1a59026da6b747e44911df787383
Wyvernhunter345 <syndix01@outlook.com>
2025-06-21 01:43:14 +0000

    [automatic commit] 2025-06-21 01:43:14 UTC 101.174.33.164

--- part1/app.js
+++ part1/app.js
@@ -106,7 +106,7 @@ let dbConnectionPool;
     ((SELECT dog_id FROM Dogs WHERE name = "Badga"), "2025-07-08 12:00:00", 40, "Botanical Gardens", "completed")`);
 
     await dbConnectionPool.execute(`INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES
-    (4, (SELECT user_id FROM Users WHERE username = "Syndix01"), 30, "Parklands", "open"),
+    (4, (SELECT user_id FROM Users WHERE username = "Isaac2014"), 30, "Parklands", "open"),
     (5, (SELECT user_id FROM Users WHERE username = "Syndix01"), 45, "Beachside Ave", "accepted")`);
   }
   catch (e)
