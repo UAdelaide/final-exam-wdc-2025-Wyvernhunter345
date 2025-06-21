3eca27ea7b18421b26668f5c6ee26bc498f3cbe8
Wyvernhunter345 <syndix01@outlook.com>
2025-06-21 05:24:36 +0000

    [automatic commit] 2025-06-21 05:24:36 UTC 101.174.33.164

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -64,7 +64,7 @@ router.post("/login", async (req, res) => {
 
         res.json({ message: "Login successful", user: rows[0] });
     } catch (error) {
-        res.status(500).json({ error: "Login failed" });
+        res.status(500).json({ error: "Login failed " + error });
     }
 });
 
