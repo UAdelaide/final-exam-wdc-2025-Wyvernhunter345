3c76e538b8ac041924cfbb97e865adec0e23a934
Wyvernhunter345 <syndix01@outlook.com>
2025-06-21 01:34:31 +0000

    [automatic commit] 2025-06-21 01:34:31 UTC 101.174.33.164

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -29,7 +29,7 @@ router.get('/walkrequests/open', async function (req,res,next) {
 
 router.get('/walkers/summary', async function (req,res,next) {
     try {
-        const [walkers] = await req.pool.execute(`SELECT username AS walker_username, COUNT(WalkRatings.rating) AS total_ratings, AVG(WalkRatings.rating) AS average_rating, `);
+        const [walkers] = await req.pool.execute(`SELECT username AS walker_username, COUNT(WalkRatings.rating) AS total_ratings, AVG(WalkRatings.rating) AS average_rating, COUNT(WalkApplications.walker_id)`);
         req.json(walkers);
     } catch (e) {
         res.status(500).json({ error: 'Failed to fetch walkers: ' + e });
